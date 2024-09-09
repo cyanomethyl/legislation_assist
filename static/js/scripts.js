e4e06b3913888e2d0a7ef9ec0b8075b2baad555c
Charles Lund <opus72no1@gmail.com>
2024-09-09 05:01:53 -0400

    Add to documentation. Finish dropdown buttons and additional CSS styling and HTML.

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,13 +1,31 @@
+function dropdownMenus(event) {
+    const dropdownButton = event.target;
+    const dropdownContainer = dropdownButton.closest(".dropdown-container");
+    if (dropdownContainer){  
+        const dropdown = dropdownContainer.querySelector(".dropdown");
+        if (getComputedStyle(dropdown).display === "none"){
+            dropdown.style.display = "flex" ;
+        }
+        else {
+            dropdown.style.display = "none";
+        }
+    }
 
-
-
-
-function dropdownMenus() {
-    
+   
 }
 
+function grabDropdownButtons() {
+    const dropdownButtons = document.querySelectorAll('.dropdown-button');
+    dropdownButtons.forEach(dropdownButton => {
+        dropdownButton.addEventListener('click', dropdownMenus);
+    });
+};
+
+
+
+
 function initializeScripts() {
-    dropdownMenus();
+    grabDropdownButtons();
 }
 
 window.addEventListener("load", () => {
