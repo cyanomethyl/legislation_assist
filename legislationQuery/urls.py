10f89a52b7bb06a2d6c30486e3fb123e1e9545fd
Charles Lund <opus72no1@gmail.com>
2024-09-10 04:45:06 -0400

    Install openai library. Begin to create response and request code to Open AI's API.

--- legislationQuery/urls.py
+++ legislationQuery/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("documentation", views.documentation, name="documentation")
+    path("documentation", views.documentation, name="documentation"),
+    path("open_ai_connect", views.open_ai_connect, name="open_ai_connect"),
 ]
--- legislationQuery/views.py
+++ legislationQuery/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import LegislationQuery
+import os
+import httpx
+import asyncio
+from openai import AsyncOpenAI
+from django.conf import settings
+from dotenv import load_dotenv
+
 
 # Create your views here.
+
+# Path to environment variable file
+load_dotenv(dotenv_path=os.path.join(settings.BASE_DIR, '.env'))
 
 def index(request):
     context = {
@@ -17,3 +27,24 @@ def documentation(request):
         'title': 'Internal Documentation | Legislation Assist',
     }
     return render(request, "legislationQuery/documentation.html", context)
+
+
+def open_ai_connect(request):
+        # Using async in order not to have to poll/auto refresh, and instead receive a real time response. 
+    client = AsyncOpenAI(
+        api_key=os.environ.get("OPEN_API_KEY"),
+        # Sets custom timeout settings via open ai's httpx client. E.g. will wait 6 seconds for a read response, 10 for a write response.
+        timeout=httpx.Timeout(120.0, read=6.0, write=10.0, connect=4.0),
+    )
+    # should remove "None"? given U WILL BE PRINTING OUT TO TEMPLATE?
+    async def main() -> None:
+        chat_completion = await client.chat.completions.create(
+            messages=[
+                {
+                    "role": "user",
+                    "content": "Say this is a test",
+                }
+            ],
+            model="gpt-4o-2024-08-06",
+        )
+    return render()
