00cb82a5e56ebc45d27e769b79c26030e379ce65
Charles Lund <opus72no1@gmail.com>
2024-09-17 16:51:19 -0400

    debug

--- legislationAssist/settings.py
+++ legislationAssist/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-if ENVIRONMENT == 'development':
+if ENVIRONMENT == 'production':
     DEBUG = True
 else:
     DEBUG = False
